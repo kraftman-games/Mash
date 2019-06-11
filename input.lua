69f8591516302b23d38784032f1e82f468da4e60
Chris Tanner <me@chris.dev>
2019-06-10 22:33:14 -0700

    move input logic out

---
+++ input.lua
@@ -0,0 +1,163 @@
+
+
+local listenButtons = {}
+listenButtons.player1 = {'leftstick', 'leftshoulder', 'triggerleft'}
+listenButtons.player2 = {'rightstick', 'rightshoulder', 'triggerright'}
+local player = require 'player'
+
+local function newController(joystick)
+  return {
+    players = {},
+    joystick = joystick,
+    id = joystick:getID()
+  }
+end
+
+
+local input = {}
+input.__index = input
+
+function input:AddController(joystick)
+  local joyID = joystick:getID()
+  if not self.controllers[joyID] then
+    self.controllers[joyID] = newController(joystick) 
+  end
+end
+
+function input:AddPlayer(controller, playerID)
+
+  local lOrR = playerID == 'player1' and 'left' or 'right'
+  local playerX = 150-math.random( 0,30 )
+  local playerY = 150+math.random( 0, 70 )
+  local player = player:Create(self.world, playerX, playerY, controller.joystick, lOrR)
+  self.world:AddPlayer(player)
+  controller.players[playerID] = player
+  for _, buttonName in pairs(listenButtons[playerID]) do
+    self.buttonListeners[controller.id..buttonName] = player
+  end
+end
+
+
+function input:RemoveController(controller)
+  if controller.joystick:isConnected() == true then
+    return
+  end
+  -- remove both of the controllers players
+  for _, player in pairs(controller.players) do
+    self.world:RemovePlayer(player)
+    for k,v in pairs(self.buttonListeners) do
+      if v == player then
+        self.buttonListeners[v] = nil
+      end
+    end
+  end
+  self.controllers[controller.id] = nil
+end
+
+function input:gamepadpressed(joystick, button)
+  -- print('gamepad pressed:')
+  self:AddController(joystick)
+  local controller = self.controllers[joystick:getID()]
+  if button == 'leftstick' and not controller.players.player1 then
+      AddPlayer(controller, 'player1')
+  end
+  
+  if button == 'rightstick' and not controller.players.player2 then
+      self:AddPlayer(controller, 'player2')
+  end
+  local playerToUpdate = self.buttonListeners[controller.id..button]
+  if playerToUpdate then
+      playerToUpdate:ButtonDown(button)
+      -- print('gamepad pressed', button)
+  end
+end
+
+function input:gamepadreleased(joystick, button)
+  self:AddController(joystick)
+  local controller = self.controllers[joystick:getID()]
+  
+  local playerToUpdate = self.buttonListeners[controller.id..button]
+  if playerToUpdate then
+    playerToUpdate:ButtonUp(button)
+  end
+end
+
+function input:gamepadaxis(joystick, axis, value)
+  print('gamepad axis:', joystick, axis, value)
+  self:AddController(joystick)
+  local controller = self.controllers[joystick:getID()]
+
+  if (axis == 'leftx' or axis == 'lefty') then
+      if not controller.players.player1 then
+          self:AddPlayer(controller, 'player1')
+      end
+      local playerToUpdate = self.buttonListeners[controller.id..axis]
+      if playerToUpdate then
+          if axis == 'leftx' then
+              -- print(value)
+              playerToUpdate:SteerX(value)
+          else
+              playerToUpdate:SteerY(value)
+          end
+      end
+
+  end
+  if axis == 'triggerleft' then
+    local playerToUpdate = self.buttonListeners[controller.id..axis]
+    if playerToUpdate then
+      if value > 0.5 then
+        playerToUpdate:ButtonDown(axis)
+      else  
+        playerToUpdate:ButtonUp(axis)
+      end
+      -- print('gamepad pressed', button)
+    end
+  end
+
+  
+  if (axis == 'rightx' or axis == 'righty') then
+      if not controller.players.player2 then
+          self:AddPlayer(controller, 'player2')
+      end
+      
+      local playerToUpdate = self.buttonListeners[controller.id..axis]
+      if playerToUpdate then
+          if axis == 'rightx' then
+              playerToUpdate:SteerX(value)
+          else
+              playerToUpdate:SteerY(value)
+          end
+      end
+  end
+end
+
+function input:update(dt)
+  local joysticks = love.joystick.getJoysticks()
+  -- add new controllers
+  for i, joystick in ipairs(joysticks) do
+    self:AddController(joystick)
+  end 
+  --remove dead controllers
+  for id, controller in pairs(self.controllers) do
+    self:RemoveController(controller)
+  end
+end
+
+function input:draw()
+  local joysticks = love.joystick.getJoysticks()
+  for i, joystick in ipairs(joysticks) do
+      love.graphics.print('name:'..joystick:getName(), 10, i * 20)
+      love.graphics.print('id:'..joystick:getID(), 250, i * 20)
+  end 
+end
+
+function input:create(world)
+  local inp = setmetatable({}, input)
+  inp.world = world
+  inp.controllers = {}
+  inp.buttonListeners = {}
+  return inp
+end
+
+
+return input
--- main.lua
+++ main.lua
@@ -5,77 +5,33 @@ local background = require('background')
 local player = require('player')
 local comet = require('comet')
 local lg = love.graphics
-local controllers = {}
-local buttonListeners = {}
-
+local inputHandler = (require 'input'):create(world)
 -- love.window.setFullscreen(true)
-
 
 -- ===============================
 DEBUG = true
 -- ===============================
 -- local newComet = comet:Create(world, 200, 200, 100)
 -- world:AddComet(newComet)
-
 function love.resize(w, h) 
   -- update the background dimensions
-end
-
-local function RemoveController(controller)
-  if controller.joystick:isConnected() == false then
-    for k, player in pairs(controller.players) do
-        world:RemovePlayer(player)
-        for k,v in pairs(buttonListeners) do
-            if v == player then
-                buttonListeners[v] = nil
-            end
-        end
-    end
-    controllers[controller.id] = nil
-  end
-end
-
-local function AddController(joystick)
-    local joyID = joystick:getID()
-    if not controllers[joyID] then
-        controllers[joystick:getID()] = {players = {}, joystick = joystick, id = joyID}
-    end
 end
 
 function love.keypressed(key) 
   if(key == 'escape') then
     love.event.quit(0)
   end
+  inputHandler:keypressed(key)
+end
+
+function love.keyreleased(key)
+  inputHandler:keyreleased(key)
 end
 
 function love.update(dt)
-  local joysticks = love.joystick.getJoysticks()
-  -- add controllers
-  for i, joystick in ipairs(joysticks) do
-      AddController(joystick)
-  end 
-  --remove controllers
-  for id, controller in pairs(controllers) do
-      RemoveController(controller)
-  end
+  inputHandler:update(dt)
   background:update(dt)
   world:Update(dt)
-end
-
-function AddPlayer(controller, playerID)
-    -- print('creating player: ', playerID)
-    local listenButtons = {}
-    listenButtons.player1 = {'leftstick', 'leftshoulder', 'triggerleft'}
-    listenButtons.player2 = {'rightstick', 'rightshoulder', 'triggerright'}
-
-    local lOrR = playerID == 'player1' and 'left' or 'right'
-    local player = player:Create(world, 50-math.random( 0,30 ), 50+math.random( 0, 70 ), controller.joystick, lOrR)
-    world:AddPlayer(player)
-    controller.players[playerID] = player
-    for k, v in pairs(listenButtons[playerID]) do
-        buttonListeners[controller.id..v] = player
-    end
-
 end
 
 -- joytick buttons pressed down 
@@ -84,93 +40,20 @@ function love.joystickpressed(joystick,button)
  end
 
 function love.gamepadpressed(joystick, button)
-    -- print('gamepad pressed:')
-    AddController(joystick)
-    local controller = controllers[joystick:getID()]
-    if button == 'leftstick' and not controller.players.player1 then
-        AddPlayer(controller, 'player1')
-    end
-    
-    if button == 'rightstick' and not controller.players.player2 then
-        AddPlayer(controller, 'player2')
-    end
-    local playerToUpdate = buttonListeners[controller.id..button]
-    if playerToUpdate then
-        playerToUpdate:ButtonDown(button)
-        -- print('gamepad pressed', button)
-    end
+  inputHandler:gamepadpressed(joystick, button)
 end
 
 function love.gamepadreleased( joystick, button )
-    AddController(joystick)
-    local controller = controllers[joystick:getID()]
-    
-    local playerToUpdate = buttonListeners[controller.id..button]
-    if playerToUpdate then
-        playerToUpdate:ButtonUp(button)
-    end
+  inputHandler:gamepadreleased(joystick, button)
 end
 
 function love.gamepadaxis( joystick, axis, value )
-    print('gamepad axis:', joystick, axis, value)
-    AddController(joystick)
-    local controller = controllers[joystick:getID()]
-
-    if (axis == 'leftx' or axis == 'lefty') then
-        if not controller.players.player1 then
-            AddPlayer(controller, 'player1')
-        end
-        local playerToUpdate = buttonListeners[controller.id..axis]
-        if playerToUpdate then
-            if axis == 'leftx' then
-                -- print(value)
-                playerToUpdate:SteerX(value)
-            else
-                playerToUpdate:SteerY(value)
-            end
-
-
-        end
-
-    end
-    if axis == 'triggerleft' then
-      local playerToUpdate = buttonListeners[controller.id..axis]
-      if playerToUpdate then
-        if value > 0.5 then
-          playerToUpdate:ButtonDown(axis)
-        else  
-          playerToUpdate:ButtonUp(axis)
-        end
-        -- print('gamepad pressed', button)
-      end
-    end
-
-    
-    if (axis == 'rightx' or axis == 'righty') then
-        if not controller.players.player2 then
-            AddPlayer(controller, 'player2')
-        end
-        
-        local playerToUpdate = buttonListeners[controller.id..axis]
-        if playerToUpdate then
-            if axis == 'rightx' then
-                playerToUpdate:SteerX(value)
-            else
-                playerToUpdate:SteerY(value)
-            end
-        end
-    end
+  inputHandler:gamepadaxis(joystick, axis, value)
 end
 
 function love.draw()
   background:draw()
   love.graphics.setColor(1,1,1,1)
   world:Draw()
-  local joysticks = love.joystick.getJoysticks()
-  for i, joystick in ipairs(joysticks) do
-      love.graphics.print('name:'..joystick:getName(), 10, i * 20)
-      love.graphics.print('id:'..joystick:getID(), 250, i * 20)
-
-  end 
-  --love.timer.sleep(0.01)
+  inputHandler:draw()
 end
